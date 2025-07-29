6015e1caf9169e4af7c73228b5f992a721081673
jshirey55 <jshirey5555@gmail.com>
2025-07-29 10:57:33 -0400

    weird fails, await errors

--- db/seed.js
+++ db/seed.js
@@ -1,10 +1,18 @@
+import { faker } from "@faker-js/faker"
 import db from "#db/client";
-
+import { createEmployee }  from "#db/queries/employees"
 await db.connect();
 await seedEmployees();
 await db.end();
 console.log("🌱 Database seeded.");
 
 async function seedEmployees() {
-  // TODO
+  for(let i = 0; i < 10; i++){
+    const employee = {
+      name: faker.person.fullName(),
+      birthday: faker.date.past({ years: 50 }),
+      salary: faker.number.int({ min: 40000, max: 60000 }),
+    };
+    await createEmployee(employee);
+  }
 }
